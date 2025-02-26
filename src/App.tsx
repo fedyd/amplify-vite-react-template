70d761e88b9f8fa25dbf7c4e7cdacd840ab4d120
unknown <federico.tomasi@gmail.com>
2025-02-26 15:38:37 +0100

    App frontend changed to Face-liveness-detector

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+/*import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
@@ -36,5 +36,86 @@ function App() {
     </main>
   );
 }
+*/
 
+import React from 'react';
+/*import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';*/
+import { Loader, ThemeProvider } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { FaceLivenessDetectorCore, AwsCredentialProvider } from '@aws-amplify/ui-react-liveness';
+ 
+const credentialProvider: AwsCredentialProvider = async () => {
+  // Fetch the credentials
+  return {
+    accessKeyId: 'accessKeyId',
+    secretAccessKey:  'secretAccessKey',
+    session   : 'session',  // optional
+    region: 'us-east-1',
+  };
+}
+
+function /*LivenessQuickStartReact*/App() {
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [createLivenessApiData, setCreateLivenessApiData] = React.useState<{
+    sessionId: string;
+  } | null>(null);
+
+  React.useEffect(() => {
+    const fetchCreateLiveness: () => Promise<void> = async () => {
+      /*
+       * This should be replaced with a real call to your own backend API
+       */
+      await new Promise((r) => setTimeout(r, 2000));
+      const mockResponse = { sessionId: 'mockSessionId' };
+      const data = mockResponse;
+
+      setCreateLivenessApiData(data);
+      setLoading(false);
+    };
+
+    fetchCreateLiveness();
+  }, []);
+
+  const handleAnalysisComplete: () => Promise<void> = async () => {
+    /*
+     * This should be replaced with a real call to your own backend API
+     */
+    const response = await fetch(
+      `/api/get?sessionId=${createLivenessApiData.sessionId}`
+    );
+    const data = await response.json();
+
+    /*
+     * Note: The isLive flag is not returned from the GetFaceLivenessSession API
+     * This should be returned from your backend based on the score that you
+     * get in response. Based on the return value of your API you can determine what to render next.
+     * Any next steps from an authorization perspective should happen in your backend and you should not rely
+     * on this value for any auth related decisions.
+     */
+    if (data.isLive) {
+      console.log('User is live');
+    } else {
+      console.log('User is not live');
+    }
+  };
+
+  return (
+    <ThemeProvider>
+      {loading ? (
+        <Loader />
+      ) : (
+        <FaceLivenessDetectorCore
+          sessionId={createLivenessApiData.sessionId}
+          region="us-east-1"
+          onAnalysisComplete={handleAnalysisComplete}
+          onError={(error) => {
+            console.error(error);
+          }}
+          config={{ credentialProvider }}
+        />
+      )}
+      
+    </ThemeProvider>
+  );
+}
 export default App;
