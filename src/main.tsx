d191d3ad71a9b976a73137d030f474fff1a89793
Federico <federico.tomasi@gmail.com>
2025-02-27 15:40:38 +0100

    Update main.tsx
    back to original file

--- src/main.tsx
+++ src/main.tsx
@@ -7,25 +7,8 @@ import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
-declare global {
-  interface Window {
-    initPlugin: (id: string) => void;
-  }
-}
-
-window.initPlugin = (id: string) => {
-  const root = ReactDOM.createRoot(
-    document.getElementById(id) as HTMLElement
-  );
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-};
-
-/*ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>*/
+  </React.StrictMode>
 );
